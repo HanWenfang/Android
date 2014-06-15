7facbb1362b340ce8d41baf80ad569cbce98011e
susan <eehanwenfang@foxmail.com>
2014-06-15 18:23:45 +0800

    button

---
+++ MISC/src/com/example/misc/ButtonActivity.java
@@ -0,0 +1,44 @@
+package com.example.misc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+/**
+ * Created by susan on 14-6-15.
+ */
+public class ButtonActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button);
+
+        buttonContainer = (LinearLayout) findViewById(R.id.layout_button_id);
+        clickme = (Button) findViewById(R.id.button_click);
+
+        clickme.setOnClickListener(new clickListener());
+    }
+
+    public class clickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId() == R.id.button_click) {
+                ImageButton temp = new ImageButton(ButtonActivity.this);
+                temp.setImageResource(android.R.drawable.btn_radio);
+                temp.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                buttonContainer.addView(temp);
+            }
+        }
+    }
+
+    private Button clickme;
+    private LinearLayout buttonContainer;
+}
